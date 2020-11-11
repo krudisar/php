9af2509c636b1545fff17daff69a29673825ba40
krudisar <krudisar@gmail.com>
2020-11-11 09:25:27 +0100

    Create index-lbaas.php

---
+++ index-lbaas.php
@@ -0,0 +1,20 @@
+<html>
+<header>
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
+</header>
+<body>
+
+<table border=4red bordercolor="blue" height="100%" width="100%"><tr><td valign="center" align="$
+    <font size=8>
+    A RESPONSE FROM BACKEND SERVER: <p>
+    <font color="red">
+        <?php
+          echo "Served by ... ", getHostByName(getHostName());
+        ?>
+    </font>
+</font></td></tr></table>
+
+</body>
+</html>
